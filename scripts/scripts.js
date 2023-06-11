0e952fbefb4bf408512299fcc3885966833eab95
Stefany <donstefany@gmail.com>
2023-06-11 13:55:40 -0300

    formulario de envio

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,39 +1,34 @@
-// Obtener todos los botones "Agregar" en la página
-const agregarBotones = document.querySelectorAll('.agregar-carrito');
+// Lista de productos en el carrito
+let carrito = [];
+
+// Obtener elementos del DOM
 const carritoProductos = document.getElementById('carritoProductos');
-const carrito = [];
 
-// Manejar el clic en el botón "Agregar"
-agregarBotones.forEach(boton => {
-  boton.addEventListener('click', () => {
-    const productoId = boton.dataset.id;
-    const productoNombre = boton.parentNode.parentNode.querySelector('.card-title').textContent;
-    const productoPrecio = boton.parentNode.parentNode.querySelector('.card-text').textContent;
+// Agregar un producto al carrito
+function agregarProductoCarrito(productoId) {
+  // Simular la obtención de los datos del producto
+  const producto = {
+    id: productoId,
+    nombre: 'Conjunto Vertu',
+    precio: 20000,
+    cantidad: 1
+  };
 
-    // Verificar si el producto ya está en el carrito
-    const productoExistente = carrito.find(producto => producto.id === productoId);
+  // Verificar si el producto ya está en el carrito
+  const productoExistente = carrito.find(item => item.id === productoId);
+  if (productoExistente) {
+    // Si el producto ya está en el carrito, incrementar la cantidad
+    productoExistente.cantidad++;
+  } else {
+    // Si el producto no está en el carrito, agregarlo
+    carrito.push(producto);
+  }
 
-    if (productoExistente) {
-      // Si el producto ya está en el carrito, incrementar la cantidad
-      productoExistente.cantidad += 1;
-    } else {
-      // Si el producto no está en el carrito, agregarlo
-      const nuevoProducto = {
-        id: productoId,
-        nombre: productoNombre,
-        precio: parseFloat(productoPrecio.replace('$', '')),
-        cantidad: 1
-      };
+  // Actualizar la interfaz del carrito
+  actualizarCarrito();
+}
 
-      carrito.push(nuevoProducto);
-    }
-
-    // Actualizar la interfaz del carrito
-    actualizarCarrito();
-  });
-});
-
-// Manejar el clic en el botón "Eliminar" del carrito
+// Eliminar un producto del carrito
 function eliminarProductoCarrito(productoId) {
   const productoIndex = carrito.findIndex(producto => producto.id === productoId);
   if (productoIndex !== -1) {
@@ -47,32 +42,30 @@ function eliminarProductoCarrito(productoId) {
 function actualizarCarrito() {
   carritoProductos.innerHTML = '';
 
+  if (carrito.length === 0) {
+    carritoProductos.innerHTML = '<p>No hay productos en el carrito</p>';
+    return;
+  }
+
   carrito.forEach(producto => {
-    const itemCarrito = document.createElement('div');
-    itemCarrito.classList.add('item-carrito');
-    itemCarrito.innerHTML = `
-        <span>${producto.nombre}</span>
-        <span>${producto.cantidad}</span>
-        <span>$${producto.precio.toFixed(2)}</span>
-        <button class="btn btn-sm btn-danger" onclick="eliminarProductoCarrito('${producto.id}')">Eliminar</button>
-      `;
-
-    carritoProductos.appendChild(itemCarrito);
+    const productoHTML = `
+      <div class="producto-carrito">
+        <h4>${producto.nombre}</h4>
+        <p>Precio: $${producto.precio}</p>
+        <p>Cantidad: ${producto.cantidad}</p>
+        <button class="btn-eliminar" onclick="eliminarProductoCarrito(${producto.id})">Eliminar</button>
+      </div>
+    `;
+    carritoProductos.innerHTML += productoHTML;
   });
-
-  calcularTotal();
 }
 
-// Calcular el total de la compra
-function calcularTotal() {
-  const total = carrito.reduce((acumulador, producto) => acumulador + (producto.precio * producto.cantidad), 0);
-  document.getElementById('totalCompra').textContent = `$${total.toFixed(2)}`;
-}
-
-// Manejar el clic en el botón "Finalizar Compra"
-const finalizarCompraBtn = document.getElementById('finalizarCompraBtn');
-finalizarCompraBtn.addEventListener('click', () => {
-  // Aquí puedes agregar la lógica para finalizar la compra
-  console.log('Compra finalizada');
-  console.log('Productos en el carrito:', carrito);
+// Event listener para los botones de agregar al carrito
+const agregarCarritoButtons = document.querySelectorAll('.agregar-carrito');
+agregarCarritoButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+    const productoId = button.dataset.id;
+    agregarProductoCarrito(productoId);
+  });
 });
